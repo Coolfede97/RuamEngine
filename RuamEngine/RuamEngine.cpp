c552c4e262defc874dbbb474206f1fb057e9412f
Coolfede97 <coolfede97@gmail.com>
2025-07-20 19:48:43 -0300

    project set up

---
+++ RuamEngine/RuamEngine.cpp
@@ -0,0 +1,12 @@
+﻿// RuamEngine.cpp : Defines the entry point for the application.
+//
+
+#include "RuamEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
