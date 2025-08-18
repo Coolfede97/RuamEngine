a761116f8b10a9776e57dedd344885ddc67ce075
Tomás Atanasoff <tomyatana@gmail.com>
2025-08-18 10:08:25 -0300

    Spaces for tabs\?

--- RuamEngine/RuamCore/Objects/Object.hpp
+++ RuamEngine/RuamCore/Objects/Object.hpp
@@ -9,53 +9,53 @@
 
 class Object {
 public:
-    Object() : m_id(s_id_count++) {}
-    using ComponentVector = std::vector<std::shared_ptr<Component>>;
-    using ComponentList = std::map<std::type_index, ComponentVector>;
+	Object() : m_id(s_id_count++) {}
+	using ComponentVector = std::vector<std::shared_ptr<Component>>;
+	using ComponentList = std::map<std::type_index, ComponentVector>;
 
-    template<class Comp>
-    Comp& addComponent() {
-        std::shared_ptr<Comp> comp = std::make_shared<Comp>(m_id);
-        const std::type_index tidx = typeid(Comp);
-        if (m_components.count(tidx) > 0) {
-            m_components[tidx].push_back(comp);
-        } else {
-            m_components.insert({tidx, ComponentVector()});
-            m_components[tidx].push_back(comp);
-        }
-        return *comp;
-    }
+	template<class Comp>
+	Comp& addComponent() {
+		std::shared_ptr<Comp> comp = std::make_shared<Comp>(m_id);
+		const std::type_index tidx = typeid(Comp);
+		if (m_components.count(tidx) > 0) {
+			m_components[tidx].push_back(comp);
+		} else {
+			m_components.insert({tidx, ComponentVector()});
+			m_components[tidx].push_back(comp);
+		}
+		return *comp;
+	}
 
-    // Returns ptr because a ref can't be null
-    // Returned pointer is non-owning
-    // TODO: Find if there's a better way
-    template<class Comp>
-    Comp* getComponent() const {
-        auto pair = m_components.find(typeid(Comp));
-        if (pair == m_components.end()) {
-            return nullptr;
-        }
-        if (pair->second.size() == 0) {
-            return nullptr;
-        }
-        return dynamic_cast<Comp*>(pair->second[0].get());
-    }
+	// Returns ptr because a ref can't be null
+	// Returned pointer is non-owning
+	// TODO: Find if there's a better way
+	template<class Comp>
+	Comp* getComponent() const {
+		auto pair = m_components.find(typeid(Comp));
+		if (pair == m_components.end()) {
+			return nullptr;
+		}
+		if (pair->second.size() == 0) {
+			return nullptr;
+		}
+		return dynamic_cast<Comp*>(pair->second[0].get());
+	}
 
-    unsigned int getId() const;
+	unsigned int getId() const;
 
 	const ComponentList& getComponents();
 
-    bool operator==(const Object& obj) {
-        return this->m_id == obj.m_id;
-    }
+	bool operator==(const Object& obj) {
+		return this->m_id == obj.m_id;
+	}
 
 	void start();
 	void update();
-    void render();
-    void imguiRender();
+	void render();
+	void imguiRender();
 private:
-    unsigned int m_id;
-    static unsigned int s_id_count;
+	unsigned int m_id;
+	static unsigned int s_id_count;
 
-    ComponentList m_components;
+	ComponentList m_components;
 };
